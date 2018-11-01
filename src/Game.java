5f9b0fd7f8d493eff6fbd2d46e0681397fb27b65
Tim <s185027@student.dtu.dk>
2018-11-01 14:02:38 +0100

    edited playTurn to implement turnSwitch edited turnSwitch to work

--- src/Game.java
+++ src/Game.java
@@ -13,69 +13,70 @@ public class Game {
     }
 
     /** Passes the turn to the next player */
-    public void turnSwitch(Player playerOne){
-        isTurn = false;
-        playerOne.isTurn = true;
+    public void turnSwitch(Player playerOne, Player playerTwo){
+        playerOne.setTurn(true);
+        playerTwo.setTurn(false);
     }
 
-    public void playTurn(Player player) {
+    public void playTurn(Player playerOne, Player playerTwo) {
         masterDice.roll();
 
         switch (masterDice.sum()) {
-            case 2: player.addToScore(250);
+            case 2: playerOne.addToScore(250);
                 System.out.println("You hit " + masterDice.sum() + ", the Tower, and got 250 points!");
-                System.out.println("Your score is now " + player.getScore());
-                player.turnSwitch(); // TODO Fix the turnSwitch for the whole switch case.
+                System.out.println("Your score is now " + playerOne.getScore());
+                turnSwitch(playerTwo, playerOne);
                 break;
-            case 3: player.subFromScore(100);
+            case 3: playerOne.subFromScore(100);
                 System.out.println("You hit " + masterDice.sum() + ", the Crater, and lost 100 points!");
-                System.out.println("Your score is now " + player.getScore());
-                player.turnSwitch();
+                System.out.println("Your score is now " + playerOne.getScore());
+                turnSwitch(playerTwo, playerOne);
                 break;
-            case 4: player.addToScore(100);
+            case 4: playerOne.addToScore(100);
                 System.out.println("You hit " + masterDice.sum() + ", the Palace gates, and got 100 points!");
-                System.out.println("Your score is now " + player.getScore());
-                player.turnSwitch();
+                System.out.println("Your score is now " + playerOne.getScore());
+                turnSwitch(playerTwo, playerOne);
                 break;
-            case 5: player.subFromScore(20);
+            case 5: playerOne.subFromScore(20);
                 System.out.println("You hit " + masterDice.sum() + ", the Cold Desert, and lost 20 points!");
-                System.out.println("Your score is now " + player.getScore());
-                player.turnSwitch();
+                System.out.println("Your score is now " + playerOne.getScore());
+                turnSwitch(playerTwo, playerOne);
                 break;
-            case 6: player.addToScore(180);
+            case 6: playerOne.addToScore(180);
                 System.out.println("You hit " + masterDice.sum() + ", the Walled city, and got 180 points!");
-                System.out.println("Your score is now " + player.getScore());
-                player.turnSwitch();
+                System.out.println("Your score is now " + playerOne.getScore());
+                turnSwitch(playerTwo, playerOne);
                 break;
             case 7:
                 System.out.println("You hit " + masterDice.sum() + ", the Monastery, and got 0 points!");
-                System.out.println("Your score is still " + player.getScore());
-                player.turnSwitch();
+                System.out.println("Your score is still " + playerOne.getScore());
+                turnSwitch(playerTwo, playerOne);
+
                 break;
-            case 8: player.subFromScore(70);
+            case 8: playerOne.subFromScore(70);
                 System.out.println("You hit " + masterDice.sum() + ", the Cold Desert, and lost 70 points!");
-                System.out.println("Your score is now " + player.getScore());
-                player.turnSwitch();
+                System.out.println("Your score is now " + playerOne.getScore());
+                turnSwitch(playerTwo, playerOne);
                 break;
-            case 9: player.addToScore(60);
+            case 9: playerOne.addToScore(60);
                 System.out.println("You hit " + masterDice.sum() + ", the Huts in the mountain, and got 60 points!");
-                System.out.println("Your score is now " + player.getScore());
-                player.turnSwitch();
+                System.out.println("Your score is now " + playerOne.getScore());
+                turnSwitch(playerTwo, playerOne);
                 break;
-            case 10: player.subFromScore(80);
+            case 10: playerOne.subFromScore(80);
                 System.out.println("You hit " + masterDice.sum() + ", the Werewall, and lost 80 points!");
-                System.out.println("Your score is now " + player.getScore());
+                System.out.println("Your score is now " + playerOne.getScore());
                 System.out.println("You get an extra turn, roll again");
                 break;
-            case 11: player.subFromScore(50);
+            case 11: playerOne.subFromScore(50);
                 System.out.println("You hit " + masterDice.sum() + ", the Pit, and lost 50 points!");
-                System.out.println("Your score is now " + player.getScore());
-                player.turnSwitch();
+                System.out.println("Your score is now " + playerOne.getScore());
+                turnSwitch(playerTwo, playerOne);
                 break;
-            case 12: player.addToScore(650);
+            case 12: playerOne.addToScore(650);
                 System.out.println("Jackpot!!! You hit " + masterDice.sum() + ", the Goldmine, and got 650 points!");
-                System.out.println("Your score is now " + player.getScore());
-                player.turnSwitch();
+                System.out.println("Your score is now " + playerOne.getScore());
+                turnSwitch(playerTwo, playerOne);
                 break;
             default:
                 System.out.println("Error, you did not roll a value between 2 and 12");
