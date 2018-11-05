c667e6accc6eceedf6712f513a3837ce16d35871
Siff <siff.ravn@gmail.com>
2018-11-05 16:38:56 +0100

    Score replaced by Balance Object

--- src/Player.java
+++ src/Player.java
@@ -1,5 +1,5 @@
 public class Player {
-    private int score;
+    private Balance balance;
     private int winCon = 3000; // TODO Flyttes senere, forbiliver for nu
     private boolean isTurn;
     private String playerName;
@@ -7,18 +7,18 @@ public class Player {
     /** Constructor, creates a instance of the class: player. */
     public Player(String playerName){
         this.playerName = playerName;
-        this.score = 1000;
+        this.balance = new Balance(1000);
     }
 
-    /** Adds an int to a players score */
-    public void addToScore(int addInt){
-        this.score = this.score + addInt;
-    }
-
-    /** Subtracts an int from a players score */
-    public void subFromScore(int subInt){
-        this.score = this.score - subInt;
-    }
+//    /** Adds an int to a players score */
+//    public void addToScore(int addInt){
+//        this.score = this.score + addInt;
+//    }
+//
+//    /** Subtracts an int from a players score */
+//    public void subFromScore(int subInt){
+//        this.score = this.score - subInt;
+//    }
 
 
     /** Boolean for what player has the turn */
