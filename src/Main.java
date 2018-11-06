fd09756e022af193d6d36b6ec2651231d762e436
Siff <siff.ravn@gmail.com>
2018-11-06 11:10:24 +0100

    jFile initialised

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,7 @@ public class Main{
 
     public static void main(String[] args) {
 
-        String jFile;
+        String jFile = "";
 
         /** Welcoming players to the game.
          * Defining the players and dice. **/
