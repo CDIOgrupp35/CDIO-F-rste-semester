3826d801b90dc042e1755c4c27d95ce13ed21752
Tim <s185027@student.dtu.dk>
2018-11-06 10:19:26 +0100

    Tested the Dice class

--- tests/DiceTest.java
+++ tests/DiceTest.java
@@ -4,24 +4,25 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class DiceTest {
-/*
+
     private Dice testDice = new Dice();
-    private Dice testDice2 = new Dice();
 
 
-
-    /** test if the roll() method generates a random number between 1 and 6, repeated 1000 times to make sure */
+    /** test if the roll() method generates TWO random numbers between 1 and 6, repeated 500 times to make sure */
     // <expected result> all tests return true
     // <actual result> all tests returned true
-  /*  @RepeatedTest(1000)
+    @RepeatedTest(500)
     void testRoll() {
         testDice.roll();
 
-        assertTrue((testDice.getFaceValue() >= 1 && testDice.getFaceValue() <= 6));
+        assertTrue((testDice.getFaceValue1() >= 1 && testDice.getFaceValue1() <= 6));
+
+        assertTrue((testDice.getFaceValue2() >= 1 && testDice.getFaceValue2() <= 6));
+
     }
-    // test if the rolls are "fair", using the same criteria as the assignment handed in this past monday.
+    /** test if the rolls are "fair", using the same criteria as the assignment handed in this past monday.
+     * Only testing getFacevalue1 since it is identical to getFaceValue2*/
     // 60000 rolls, 4% margin of error.
-    // Tested 500 times to eliminate any element of luck.
     // This test the same as our group's answer of the earlier assignment this week.
     // <expected result> all rolls are spread evenly and fairly within a 4% margin of error.
     // <actual result> successful spread of values.
@@ -35,7 +36,7 @@ class DiceTest {
 
         for(int i = 0; i < 60000; i++) {
             testDice.roll();
-            int rollResult = testDice.getFaceValue();
+            int rollResult = testDice.getFaceValue1();
 
             switch(rollResult) {
                 case 1:
@@ -70,66 +71,13 @@ class DiceTest {
 
     }
 
-    //tests the chances of rolling doubles
-    //using 2 fair dice, the chances of rolling doubles should be around 16%
-    //I'm expecting between 9000 and 11000 rolls are doubles if I roll the dice 60000 times.
-    //test repeated 100 times to lessen the chances of luck.
-    //<expected result> between 9000 and 11000 rolls are doubles
-    //<actual result> between 9000 and 11000 rolls are doubles
+    /** Testingg the addition of the Dice.Sum method with set-values  */
     @Test
-    void testDoubleFairness () {
-        int doubles, singles;
-        doubles = singles = 0;
+    void testSum() {
+        testDice.setFaceValue1(5);
+        testDice.setFaceValue2(4);
 
-        for(int i = 1; i < 60000; i++) {
-            testDice.roll();
-            testDice2.roll();
-
-            if(testDice.getFaceValue() == testDice2.getFaceValue()) {
-                doubles++;
-            }
-            else {
-                singles++;
-            }
-        }
-
-        assertTrue((doubles >= 9000) && (doubles <= 11000));
-
-
+        assertEquals(9,testDice.sum());
     }
 
-    //test of the equals method. Uses 2 tests, one for equal rolls, the other for unequal rolls.
-    //FIRST TEST
-    //<summary> Testing testDice faceValue with itself
-    //<expected result> returns true
-    //<actual result> returned true
-    //SECOND TEST
-    //<summary> Testing testDice faceValue with testDice2
-    //<expected result> returns false
-    //<actual result> returned false
-    @Test
-    void testEquals() {
-
-        //test 1
-        testDice.roll();
-        assertTrue(testDice.equals(testDice));
-
-        //test 2
-        testDice2.roll();
-        while(testDice2.getFaceValue() == testDice.getFaceValue()) {
-            testDice2.roll();
-        }
-        assertFalse(testDice.equals(testDice2));
-
-    }
-
-    //test of the getSum() method, that adds together the facevalues of 2 dice objects.
-    //<expected result> the sum of the faceValues of testDice and testDice2
-    //<actual result> the sum of the faceValues of testDice and testDice2
-    @Test
-    void testGetSum() {
-        testDice.roll();
-        testDice2.roll();
-        assertEquals(testDice.getSum(testDice2), testDice.getFaceValue() + testDice2.getFaceValue());
-    }*/
 }
