8e2c1620830a10a79e280718aea6172f38bd93be
Siff <siff.ravn@gmail.com>
2018-11-06 12:09:38 +0100

    GameIntegrationsTest class added

---
+++ tests/GameIntegrationTest.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameIntegrationTest {
+
+    @Test
+    void turnSwitch() {
+    }
+
+    @Test
+    void playTurn() {
+    }
+
+    @Test
+    void winGame() {
+    }
+
+    @Test
+    void showTurn() {
+    }
+
+    @Test
+    void playing() {
+    }
+
+    @Test
+    void getPlay() {
+    }
+}
